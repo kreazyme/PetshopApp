f9aa3e0cea7cfb2c1839f6dc2e4afc814ed38a0a
Duc Thong <thongtd@dipro.vn>
2022-10-08 21:40:32 +0700

    config project :rocket:

---
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const LoginScreenComp = () => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
+
+export const LoginScreen = React.memo(LoginScreenComp)
+
+const styles = StyleSheet.create({
+    container: {
+    },
+});
