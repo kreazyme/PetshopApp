eea49c7f6788f32262c41d024e46be8f909d9ab6
Duc Thong <thongtd@dipro.vn>
2022-10-08 21:56:25 +0700

    config project :tada:

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 const LoginScreenComp = () => {
     return (
         <View style={styles.container}>
-
+            <Text style={{ fontSize: 30, color: "red" }}>132321</Text>
         </View>
     );
 }
@@ -13,5 +13,6 @@ export const LoginScreen = React.memo(LoginScreenComp)
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: "white"
     },
 });
