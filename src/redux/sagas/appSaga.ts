70b8d3219191e36d0a5d2c2b1a559dc0a2ca3754
Duc Thong <thongtd@dipro.vn>
2022-10-23 23:30:24 +0700

    fix: Add data to store and reducer

--- src/redux/sagas/appSaga.ts
+++ src/redux/sagas/appSaga.ts
@@ -1,7 +1,7 @@
 import { CHANGE_APP_MODE } from './../actions/actionTypes';
-import { put } from "redux-saga/effects"
+import { call, put } from "redux-saga/effects"
 
 export default function* (action: any) {
-    console.log("Saga 123: " + action);
-    yield put({ type: CHANGE_APP_MODE, payload: action.payload });
+    // console.log("Saga 123: " + JSON.stringify(action));
+    // yield put({ type: CHANGE_APP_MODE, payload: action.payload });
 }
