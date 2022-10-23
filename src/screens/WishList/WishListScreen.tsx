79b91a7e75dde5e893172679cda06abb9ee5f184
Duc Thong <thongtd@dipro.vn>
2022-10-23 23:30:24 +0700

    fix: Add data to store and reducer

--- src/screens/WishList/WishListScreen.tsx
+++ src/screens/WishList/WishListScreen.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { CHANGE_APP_MODE } from "../../redux/actions/actionTypes";
 
 const WishListScreenComp = () => {
+
+    const dispatch = useDispatch()
+    const darkMode = useSelector(state => state?.appReducer.darkMode);
+    React.useEffect(() => {
+        console.log("darkMode " + JSON.stringify(darkMode))
+    }, [darkMode])
+
     return (
         <View style={styles.container}>
-            <Text>WishList Screen</Text>
+            <TouchableOpacity
+                onPress={() => {
+                    dispatch({
+                        type: CHANGE_APP_MODE,
+                        payload: !darkMode
+                    })
+                }}
+            >
+                <Text>WishList Screen</Text>
+            </TouchableOpacity>
         </View>
     );
 }
